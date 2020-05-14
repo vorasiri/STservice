ec780a5db6398e8ae6b4cbd179f45c80c3f1dca6
Vorasiri <s6101012630143@email.kmutnb.ac.th>
2020-05-14 18:16:17 +0700

    Add forms: html files

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'login.html'),
     protocol: 'file:',
     slashes: true
   }));
