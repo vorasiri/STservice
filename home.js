2ecc6408846d6281ec304f37875ddb367a6e40ae
Kongpob-R <kongpop.rak@hotmail.co.th>
2020-05-16 03:26:18 +0700

    add feature to replace main with coresponding form when click add button

--- home.js
+++ home.js
@@ -22,7 +22,71 @@ Array.from(allNavButton).forEach(navButton => {
     fs.readFile('info_page.html', function (err, data) {
       console.log(e.target.innerText)
       document.getElementById('mainContent').innerHTML = data.toString();
-      document.getElementById('pageHeader').innerText = e.target.innerText;
+      if (contains(e.target.innerHTML, ['แอร์', 'เครื่องทำน้ำอุ่น', 'จานดาวเทียม'])) {
+        document.getElementById('pageHeader').innerHTML = 'ข้อมูลการติดตั้ง ' + e.target.innerText;
+      }
+      else {
+        document.getElementById('pageHeader').innerHTML = e.target.innerText;
+      }
+      document.getElementById('addButton').addEventListener('click', function () {
+        callHtmlFile(headerToForm(e.target.innerText))
+      });
     })
   })
 });
+
+function contains(target, pattern) {
+  var value = 0;
+  pattern.forEach(function (word) {
+    value = value + target.includes(word);
+  });
+  return (value === 1)
+}
+
+// call html file to id 'mainContent'
+function callHtmlFile(filename) {
+  console.log(filename)
+  fs.readFile(filename.toString(), function (err, data) {
+    document.getElementById('mainContent').innerHTML = data.toString();
+  })
+};
+
+// put the right forms.html to current page add button
+function headerToForm(pageHeader) {
+  console.log(pageHeader)
+  var form;
+  if (pageHeader.includes('ข้อมูลบริการซ่อม')) {
+    form = 'forms/repairing_form.html';
+  }
+  else if (pageHeader.includes('ข้อมูลการคืนสินค้า')) {
+    form = 'forms/returning_form.html';
+  }
+  else if (pageHeader.includes('ข้อมูลการจัดส่งสินค้า')) {
+    form = 'forms/delivery_form.html';
+  }
+  else if (pageHeader.includes('จานดาวเทียม')) {
+    form = 'forms/installation_sat_form.html';
+  }
+  else if (pageHeader.includes('แอร์')) {
+    form = 'forms/installation_ac_form.html';
+  }
+  else if (pageHeader.includes('เครื่องทำน้ำอุ่น')) {
+    form = 'forms/installation_wh_form.html';
+  }
+  else if (pageHeader.includes('รายชื่อยี่ห้อ')) {
+    form = 'forms/brand_form.html';
+  }
+  else if (pageHeader.includes('รายชื่อลูกค้า')) {
+    form = 'forms/customer_form.html';
+  }
+  else if (pageHeader.includes('รายชื่อพนักงาน')) {
+    form = 'forms/staff_form.html';
+  }
+  else if (pageHeader.includes('รายชื่อศูนย์บริการ')) {
+    form = 'forms/service_partner_form.html';
+  }
+  else if (pageHeader.includes('รายการอะไหล่')) {
+    // form = '.html';
+  }
+  return form;
+};
