02296f7dc6fcf74ab3239e230d3bf353336012b1
Kongpob-R <kongpop.rak@hotmail.co.th>
2020-06-11 22:04:21 +0700

    edit displaying years from AD to BE

--- home.js
+++ home.js
@@ -178,8 +178,32 @@ function callHtmlFile(filename) {
     if (document.getElementById('appointmentDate')) {
       $.datetimepicker.setLocale('th');
       document.getElementById('appointmentDate').type = 'text'
-      $('#appointmentDate').datetimepicker();
+      $(function () {
 
+        var thaiYear = function (ct) {
+          var leap = 3;
+          var dayWeek = ["พฤ.", "ศ.", "ส.", "อา.", "จ.", "อ.", "พ."];
+          if (ct) {
+            var yearL = new Date(ct).getFullYear() - 543;
+            leap = (((yearL % 4 == 0) && (yearL % 100 != 0)) || (yearL % 400 == 0)) ? 2 : 3;
+            if (leap == 2) {
+              dayWeek = ["ศ.", "ส.", "อา.", "จ.", "อ.", "พ.", "พฤ."];
+            }
+          }
+          this.setOptions({
+            i18n: { th: { dayOfWeek: dayWeek } }, dayOfWeekStart: leap,
+          })
+        };
+
+        $("#appointmentDate").datetimepicker({
+          format: 'd/m/Y H:i',  // กำหนดรูปแบบวันที่ ที่ใช้ เป็น 00-00-0000            
+          lang: 'th',  // แสดงภาษาไทย
+          onChangeMonth: thaiYear,
+          onShow: thaiYear,
+          yearOffset: 543,  // ใช้ปี พ.ศ. บวก 543 เพิ่มเข้าไปในปี ค.ศ
+          closeOnDateSelect: true,
+        });
+      });
     }
   })
 };
